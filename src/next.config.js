e3d018ffbcde9bdb7f8a7a9e716326859068a2e2
Glenn Bosch <Bosch.RGM@gmail.com>
2025-06-27 15:25:05 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.js
@@ -0,0 +1,49 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+
+const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  webpack: (config) => {
+    // Tell webpack to ignore watching the 'tools' directory
+    // This prevents the dev server from restarting in a loop when we generate tool components
+    const ignored = [].concat(config.watchOptions.ignored || []);
+    ignored.push(path.resolve(__dirname, 'src/components/tools'));
+    config.watchOptions.ignored = ignored;
+    return config;
+  },
+};
+
+module.exports = nextConfig;
