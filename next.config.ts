3ea84c92a86078875b502f86d637014975f0d1b7
Glenn Bosch <Bosch.RGM@gmail.com>
2025-06-29 14:10:08 +0000

    Oké, als de app eerder wel werkte is dit waarschijnlijk een deployment o

--- next.config.ts
+++
@@ -1,3 +0,0 @@
-// This file is obsolete and can be safely removed. 
-// The configuration has been moved to next.config.js to resolve startup issues with Next.js.
-// Deleting this file to prevent any potential conflicts.
