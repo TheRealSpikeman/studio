b0ca1f64bee88e08bb1f5e958026335bcb46f50e
Glenn Bosch <Bosch.RGM@gmail.com>
2025-06-29 22:31:28 +0000

    Probleem is gevonden, we moeten de directory structuur herstellen: PERFE

---
+++ app/src/app/dashboard/admin/documentation/layout.tsx
@@ -0,0 +1,8 @@
+// src/app/dashboard/admin/documentation/layout.tsx
+export default function DocumentationLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return <>{children}</>;
+  }
