e9cc7a2cfbbde2fe880caebd91278b873482a77a
Glenn Bosch <Bosch.RGM@gmail.com>
2025-06-29 12:38:41 +0000

    Nog steeds niks

--- next.config.ts
+++ next.config.ts
@@ -1,2 +1,3 @@
-// This file is obsolete. The configuration has been moved to next.config.js
-// to resolve startup issues with Next.js.
+// This file is obsolete and can be safely removed. 
+// The configuration has been moved to next.config.js to resolve startup issues with Next.js.
+// Deleting this file to prevent any potential conflicts.
