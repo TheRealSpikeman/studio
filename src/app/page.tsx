3b4afc9e3cf52a35e7a9c5999e7bc3d9a8a26494
Glenn Bosch <Bosch.RGM@gmail.com>
2025-06-30 05:33:03 +0000

    firebase serve # of npm run dev # of welke command je normaal gebruikt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 // src/app/page.tsx
 "use client";
+import { Header } from '@/components/layout/header';
 
 export default function LandingPage() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold text-center mb-8">
-          Firebase Studio - Test Page
-        </h1>
-        <p className="text-center text-gray-600">
-          Basic routing works!
-        </p>
+    <>
+      <Header />
+      <div className="p-8">
+        <h1>Header component loaded!</h1>
       </div>
-    </div>
+    </>
   );
 }
